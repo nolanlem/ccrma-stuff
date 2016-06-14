2360eb9fb097d2d675e426afcefdedd2df1ddfb6
Nolan Lem <nolan.lem@gmail.com>
2016-06-13 21:28:06 -0700

    june 13 2

--- oscillator.py
+++ oscillator.py
@@ -13,25 +13,23 @@ class oscillator(object):
 		self.phase = phase 
 	def addFreq(self, freq, ang, kn): 
 		""" add initilized freq to phase"""
-		self.phase += freq
-		self.phase += kn*np.sin(ang-self.phase)
-		self.phase %= (2*np.pi) 
+		#if self.phase >= (2*np.pi):
+		#	self.phase = 0 
+		self.phase += freq + kn*np.sin(ang-self.phase)
 		return self.phase 
 	def showPhase(self): 
 		print self.phase 
 		return self.phase
 
 plt.ion()
-fig = plt.figure() 
-plt.axis([0, 2*np.pi, 0, 2*np.pi]);
-
+fig,axarr = plt.subplots(1,sharex=True) 
 myosc = oscillator(0, 0.0) 
 
 oscs = []
 ifreqs = [] 
-
+kn = 0.8  # no coupling, for tesing
 for i in range(0,10): 
-	ifreqs.append(random.random())
+	ifreqs.append(random.random()/10)
 
 
 # create 10 new oscillators
@@ -39,7 +37,11 @@ for i in range(0,10):
 	oscs.append(oscillator(i, random.random()*2*np.pi)) # random initial phases 
 
 
+
 while(True):
+	plt.ylim([-1.1,1.1])
+	plt.xlim([-1.1, 1.1])
+	axarr.set_ylim([-1.1,1.1])
 	tempPh = []
 	r = 0
 	for osc in oscs:
@@ -47,13 +49,29 @@ while(True):
 		myphase = osc.showPhase()
 		r += (np.cos(myphase) + 1j*np.sin(myphase) )
 
-	plt.plot(tempPh) 
+	tempPh = np.array(tempPh)
+	rcirc = np.exp(-1j*tempPh)
 
 	r /= len(oscs) 
-	rang = np.arctan(np.real(r)/np.imag(r)) 
+	rang = np.arctan(np.imag(r)/np.real(r)) 
+
+	if (np.real(r) < 0 and np.imag(r) > 0):
+		rang  += np.pi 
+	elif (np.real(r) < 0 and np.imag(r) < 0): 
+		rang += np.pi 
+	elif (np.real(r) > 0 and np.imag(r) < 0): 
+		rang += 2*np.pi
 	
 	for i in range(0,10):
-		oscs[i].addFreq(ifreqs[i], rang, 0.01) 
+		oscs[i].addFreq(ifreqs[i], rang, kn) 
+
+	x0,x1 =axarr.get_xlim()
+	y0,y1 = axarr.get_ylim() 
+	axarr.set_aspect(abs(x1-x0)/abs(y1-y0)) 
+	axarr.grid(b=True, which='major', color='k', linestyle='--') 
+
+	plt.scatter(np.real(rcirc), np.imag(rcirc)) 
+	plt.pause(0.0001)
 
 	#sumPh = sum(tempPh) 
 	#sumPh /= len(oscs) 
@@ -62,8 +80,8 @@ while(True):
 
 	#for x,y in itertools.combinations(tempPh, 2): 
 	#	print x,y
-
-	time.sleep(0.3)
+	plt.cla()
+	time.sleep(0.06)
 	print "\n \n "
 
 
